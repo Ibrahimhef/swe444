79babacb339ec748a43c0e481f47c170a24d2fbd
Abdullah <aalzughibi2@gmail.com>
2020-10-09 17:32:55 +0300

    display meal method

--- lib/Services/database.dart
+++ lib/Services/database.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:swe444/models/meals.dart';
 import 'package:swe444/models/profile.dart';
 
 class DatabaseService {
@@ -14,17 +15,45 @@ class DatabaseService {
         .setData({'name': full_name, "email": email, "password": password});
   }
 
-  Future insertMeals(int category, String title, String ingredients,
-      String step, int duration, String email, String imageURL) async {
+// method for insert meal info into database
+  Future insertMeals(
+      int category,
+      String title,
+      String description,
+      String ingredients,
+      String step,
+      int duration,
+      String email,
+      String imageURL) async {
     return await mealCollection.document(uid).setData({
       "category": category,
       'title': title,
+      'description': description,
       'ingredients': ingredients,
       'steps': step,
       'duration': duration,
       'email': email,
       'imageURL': imageURL
     });
+  }
+
+// method for display list of meals info into screen(private)
+  List<Meal> _mealListFromSnapShot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc) {
+      return Meal(
+          title: doc.data['title'] ?? '',
+          description: doc.data['description'] ?? '',
+          category: doc.data['category'] ?? 0,
+          ingredients: doc.data['ingredients'] ?? '',
+          step: doc.data['steps'] ?? '',
+          duration: doc.data['duration'] ?? 0,
+          email: doc.data['email'] ?? '',
+          imageURL: doc.data['imageURL'] ?? '');
+    }).toList();
+  }
+
+  Stream<List<Meal>> get meals {
+    return userCollection.snapshots().map(_mealListFromSnapShot);
   }
 
   List<profile> _userListFromSnapShot(QuerySnapshot snapshot) {
--- lib/SubCategories.dart
+++ lib/SubCategories.dart
@@ -1,7 +1,10 @@
+import 'package:provider/provider.dart';
 import 'package:swe444/List_info.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:swe444/Services/database.dart';
+import 'package:swe444/models/meals.dart';
 import 'package:swe444/profile.dart';
 import 'package:swe444/NewRecipe.dart';
 
@@ -741,68 +744,71 @@ class catogory extends State<Catogory> {
       if (ListOfCateogry[i].caterogry == category)
         ListOfCurrentCateogry.add(ListOfCateogry.elementAt(i));
     }
-    return Scaffold(
-      backgroundColor: Color(0xffF5F5F5),
-      bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: Color.fromRGBO(242, 171, 39, 1),
-        height: 50,
-        items: [
-          Image.asset('assets/Juice_icon@3x.png'),
-          Image.asset('assets/Appetizers_icon@3x.png'),
-          Image.asset('assets/Main Dishes_icon@3x.png'),
-          Image.asset('assets/Salads_icon@3x.png'),
-          Image.asset('assets/Soup_icon@3x.png'),
-          Image.asset('assets/Sweets _icon@3x.png'),
-          Icon(Icons.person)
-        ],
-        onTap: (index) {
-          setState(
-            () {
-              if (index < 6) {
-                category = index;
-                for (int i = 0; i < ListOfCateogry.length; i++) {
-                  if (ListOfCateogry[i].caterogry == category)
-                    ListOfCurrentCateogry.add(ListOfCateogry.elementAt(i));
+    return StreamProvider<List<Meal>>.value(
+      value: DatabaseService().meals,
+      child: Scaffold(
+        backgroundColor: Color(0xffF5F5F5),
+        bottomNavigationBar: CurvedNavigationBar(
+          backgroundColor: Color.fromRGBO(242, 171, 39, 1),
+          height: 50,
+          items: [
+            Image.asset('assets/Juice_icon@3x.png'),
+            Image.asset('assets/Appetizers_icon@3x.png'),
+            Image.asset('assets/Main Dishes_icon@3x.png'),
+            Image.asset('assets/Salads_icon@3x.png'),
+            Image.asset('assets/Soup_icon@3x.png'),
+            Image.asset('assets/Sweets _icon@3x.png'),
+            Icon(Icons.person)
+          ],
+          onTap: (index) {
+            setState(
+              () {
+                if (index < 6) {
+                  category = index;
+                  for (int i = 0; i < ListOfCateogry.length; i++) {
+                    if (ListOfCateogry[i].caterogry == category)
+                      ListOfCurrentCateogry.add(ListOfCateogry.elementAt(i));
+                  }
+                } else {
+                  Navigator.push(
+                      context,
+                      new MaterialPageRoute(
+                          builder: (context) =>
+                              new Profile(weidth, height, ListOfCateogry)));
                 }
-              } else {
-                Navigator.push(
-                    context,
-                    new MaterialPageRoute(
-                        builder: (context) =>
-                            new Profile(weidth, height, ListOfCateogry)));
-              }
-            },
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-              context,
-              new MaterialPageRoute(
-                  builder: (context) =>
-                      new AddPage(weidth: weidth, height: height)));
-        },
-        backgroundColor: Color(0xfff2780c),
-        child: Icon(Icons.add),
-      ),
-      appBar: AppBar(
-        title: Center(
-          child: Text(
-            cat1[category],
-            style: TextStyle(
-                color: Colors.black,
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'OleoScript'),
-          ),
+              },
+            );
+          },
         ),
-        backgroundColor: Color(0xfff2b705),
-        // elevation: 0,
-        // bottomOpacity: 0,
-      ),
-      body: Container(
-        child: ListInfo(ListOfCurrentCateogry, weidth, height),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+                context,
+                new MaterialPageRoute(
+                    builder: (context) =>
+                        new AddPage(weidth: weidth, height: height)));
+          },
+          backgroundColor: Color(0xfff2780c),
+          child: Icon(Icons.add),
+        ),
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              cat1[category],
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'OleoScript'),
+            ),
+          ),
+          backgroundColor: Color(0xfff2b705),
+          // elevation: 0,
+          // bottomOpacity: 0,
+        ),
+        body: Container(
+          child: ListInfo(ListOfCurrentCateogry, weidth, height),
+        ),
       ),
     );
   }
